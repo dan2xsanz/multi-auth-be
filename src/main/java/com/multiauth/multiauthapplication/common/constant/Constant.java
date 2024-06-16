83f82597275362c35cddad836da90064c028697c
Dan Lester Sanz <dsanz@csyoutsourcing.com>
2024-06-16 11:36:29 +0800

    add constance folder

---
+++ src/main/java/com/multiauth/multiauthapplication/common/constant/Constant.java
@@ -0,0 +1,16 @@
+package com.multiauth.multiauthapplication.common.constant;
+
+public class Constant {
+
+    public static final String PROD_IMAGE_NAME_1 = "Product Image 1";
+
+    public static final String PROD_IMAGE_NAME_2 = "Product Image 2";
+
+    public static final String PROD_IMAGE_NAME_3 = "Product Image 3";
+
+    public static final String PROD_IMAGE_NAME_4 = "Product Image 4";
+
+    public static final String IMAGE = "Image";
+
+    public static final String JPEG = "jpeg";
+}
