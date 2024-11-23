1fb8116edbaa9415dc4dd922d5ac3454bb466c3a
danlessanz <danles.sanz@gmail.com>
2024-11-23 14:41:53 +0800

    apply staging deployment

--- src/main/java/com/multiauth/multiauthapplication/config/cors/CorsConfig.java
+++ src/main/java/com/multiauth/multiauthapplication/config/cors/CorsConfig.java
@@ -13,45 +13,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
-//
-//    @Autowired
-//    private Environment environment;
-//
-//    /**
-//     * CorsConfigurationSource allows web servers to specify who can access its resources
-//     *
-//     * @return -Return registered configuration response
-//     */
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowCredentials(true);
-//        String[] allowedOrigins = environment.getProperty("allowed.origin").split(",");
-//        for (String allowedOrigin : allowedOrigins) {
-//            configuration.addAllowedOrigin(allowedOrigin.trim());
-//        }
-//        configuration.addAllowedHeader("*");
-//        configuration.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", configuration);
-//
-//        return source;
-//    }
-//
-//    /**
-//     * Configure a filter instance and define its order of execution in the filter chain
-//     *
-//     * @return -Return filtered bean responses
-//     */
-//    @Bean
-//    public FilterRegistrationBean<CorsFilter> corsFilter() {
-//        final CorsConfigurationSource corsConfigurationSource = corsConfigurationSource();
-//        final CorsFilter filter = new CorsFilter(corsConfigurationSource);
-//
-//        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(filter);
-//        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//        return bean;
-//    }
+
 @Autowired
 private Environment environment;
 
